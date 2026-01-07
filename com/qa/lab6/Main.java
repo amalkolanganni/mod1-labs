901141095fe65658df1a3868b3c8bf4b899a10a2
Amal Kolanganni <akolanganni@gmail.com>
2026-01-07 10:04:07 +0000

    Update Main.java

--- com/qa/lab6/Main.java
+++ com/qa/lab6/Main.java
@@ -4,7 +4,7 @@ public class Main {
     
     public static void main(String[] args) {
 
-    System.out.println("Hello from the feature branch!");
+    System.out.println("Hello from the test branch!");
     }
 
 }
